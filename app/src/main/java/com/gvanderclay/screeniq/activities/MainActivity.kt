95687004c11be474debba6cb43703c6f7236bb1a
Gage Vander Clay <gvanderclay@gmail.com>
2018-04-20 20:58:14 -0400

    Add button to start tracking

--- app/src/main/java/com/gvanderclay/screeniq/activities/MainActivity.kt
+++ app/src/main/java/com/gvanderclay/screeniq/activities/MainActivity.kt
@@ -2,14 +2,34 @@ package com.gvanderclay.screeniq.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.gvanderclay.screeniq.R
 import com.gvanderclay.screeniq.services.TrackingService
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var startButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        TrackingService.startActionInitService(this, 1000)
+        startButton = findViewById(R.id.start_button)
+
+        onStopClicked()
+    }
+
+    fun onStartClicked() {
+        startButton.text = getString(R.string.stop_tracking)
+        startButton.setOnClickListener(null)
+        startButton.setOnClickListener {
+            onStopClicked()
+        }
+    }
+
+    fun onStopClicked() {
+        startButton.text = getString(R.string.start_tracking)
+        startButton.setOnClickListener(null)
+        startButton.setOnClickListener {
+            onStartClicked()
+        }
     }
 }
